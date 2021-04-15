171200edee03080be584cba82d508ebcbb14287a
Genaro V Salinas <gvsalinas3@gmail.com>
2021-04-14 18:50:41 -0700

    updated test

--- src/tests/App.test.tsx
+++ src/tests/App.test.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
 import '@testing-library/jest-dom';
-import App from '../App';
+import Home from '../components/Home';
 
 test('renders title text', () => {
-  render(<App />);
+  render(<Home profilePhoto='adf' />);
   const linkElement = screen.getAllByText(/Genaro V\. Salinas/i);
   expect(linkElement[0]).toBeInTheDocument();
 });
